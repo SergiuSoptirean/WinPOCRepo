76b8faa06e3ef6bab845e89a4567a6abb81e628c
SergiuSoptirean <sergiu.soptirean@hotmail.com>
2021-08-20 16:08:43 +0300

    Added net core solution with test

---
+++ FLAUIPOC/FLAUINetCore/TestClassWinApp.cs
@@ -0,0 +1,48 @@
+﻿using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.UIA3;
+using NUnit.Framework;
+using System.Linq;
+using System.Threading;
+
+namespace FLAUINetCore
+{
+    public class TestClassWinApp
+    {
+        Application _app;
+        Window _mainWindow;
+        Window[] _allWindowsList;
+        UIA3Automation _automation;
+
+        private AutomationElement FindElementByName(string text)
+        {
+            var element = _mainWindow.FindFirstDescendant(cf => cf.ByName(text));
+            return element;
+        }
+
+        private AutomationElement FindElementByID(string text)
+        {
+            var element = _mainWindow.FindFirstDescendant(cf => cf.ByAutomationId(text));
+            return element;
+        }
+
+        [Category("Notepad")]
+        [Test]
+        public void StartApp()
+        {
+            _app = Application.Launch("notepad.exe");
+
+            _automation = new UIA3Automation();
+            _mainWindow = _app.GetMainWindow(_automation);
+            _mainWindow.AsTextBox().Text = "This should appear in Notepad";
+            var FileMenu = _mainWindow.FindAllDescendants(x => x.ByName("File")).First();
+            FileMenu.Click();
+
+            var fileMenuItem = _mainWindow.FindAllDescendants(x => x.ByName("Save")).First();
+
+            Thread.Sleep(3000);
+            _app.Close();
+        }
+
+    }
+}
